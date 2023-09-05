316691cf9e63ac0be2567cb2d1242f251b702aa8
Umukoro Fortune <fortuneumukoro@yahoo.com>
2023-09-05 15:23:45 +0100

    first commit

---
+++ external.js
@@ -0,0 +1,20 @@
+let number = 2;
+console.log(typeof number);
+
+let isRich = true;
+let isHtml = false;
+console.log(typeof isRich);
+console.log(typeof isHtml);
+
+let partsentenceone = "This is a ";
+let partsentencetwo = "complete sentence";
+console.log(partsentenceone+partsentencetwo);
+
+let name = "My name is fortune.";
+let program = "full stack developer";
+let ans = name +" " + "i'm a" + " " + program;
+
+console.log(ans);
+
+let output = 2 + 4;
+console.log(output);
